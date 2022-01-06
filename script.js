43a538633f2b4df36ed9643be395c9eabf30a69c
Daniel Bowen <djbowen95@gmail.com>
2022-01-06 13:12:54 +0000

    have function declare computer's choice of rock/paper/scissors

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === "rock"|| userInput === "paper"|| userInput === "scissors") {
-      console.log("Good choice.");
-      return userInput;
-    } else {
-      console.log(`You can't fight with a ${userInput}, it's too dangerous!`);
-      console.log("Please choose something more appropriate.");
-    }
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    console.log("Good choice.");
+    return userInput;
+  } else {
+    console.log(`You can't fight with a ${userInput}, it's too dangerous!`);
+    console.log("Please choose something more appropriate.");
   }
+};
+
+const getComputerChoice = () => {
+  let computerRandNum = Math.random() * 3;
+  computerRandNum = Math.floor(computerRandNum);
+  if (computerRandNum === 0) {
+    return "rock";
+  } else if (computerRandNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
